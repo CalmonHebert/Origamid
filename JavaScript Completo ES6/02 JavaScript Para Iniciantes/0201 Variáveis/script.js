709560173a5746263eb68008add15ad6dafabce0
Calmon Hebert <102420193+CalmonHebert@users.noreply.github.com>
2024-02-05 20:05:23 -0300

    Create script.js

---
+++ JavaScript Completo ES6/02 JavaScript Para Iniciantes/0201 Variáveis/script.js
@@ -0,0 +1,23 @@
+var nome = 'André';
+var idade = 28;
+
+console.log(nome, idade);
+
+var preço = 25;
+var totalComprado = 5;
+var totalPreço = totalComprado * preço;
+
+console.log(totalPreço);
+
+var sobrenome = "Rafael", cidade = 'Rio de Janeiro';
+
+console.log(sobrenome, cidade);
+
+var comida;
+console.log(comida);
+comida = 'Pizza'
+console.log(comida);
+
+var time = 'Vasco';
+time = 'Flamento'
+console.log(time);
