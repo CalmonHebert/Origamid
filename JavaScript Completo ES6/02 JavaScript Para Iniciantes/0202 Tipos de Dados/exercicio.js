0650c74f0883e274aa0573b12ce962bd35a09049
Calmon Hebert <102420193+CalmonHebert@users.noreply.github.com>
2024-02-05 20:44:34 -0300

    Update exercicio.js

---
+++ JavaScript Completo ES6/02 JavaScript Para Iniciantes/0202 Tipos de Dados/exercicio.js
@@ -0,0 +1,16 @@
+// Declare uma variável contendo uma string
+let banda = "Metallica";
+// Declare uma variável contendo um número dentro de uma string
+let codigo = 'COD2005';
+// Declare uma variável com a sua idade
+let idade = 34;
+// Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+let nome = 'Calmon';
+let sobrenome = 'Hebert'
+let nomeCompleto = nome + sobrenome;
+
+// Coloque a seguinte frase em uma variável: It's time
+let frase = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
