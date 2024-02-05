2b99ccb5193da40e62deabca73cd75a7eb4f3095
Calmon Hebert <102420193+CalmonHebert@users.noreply.github.com>
2024-02-05 20:33:16 -0300

    Create script.js

---
+++ JavaScript Completo ES6/02 JavaScript Para Iniciantes/0202 Tipos de Dados/script.js
@@ -0,0 +1,14 @@
+var nome = 'Calmon';
+console.log(typeof nome);
+
+let idade = 34;
+console.log(typeof idade);
+
+let sobrenome = "Hebert"
+let nomeCompleto = nome + sobrenome
+console.log(nomeCompleto);
+
+var gols = 1000
+var frase = 'Romario fez ' + gols + ' gols'
+console.log(frase);
+console.log(`Romario fez ${gols + 500} gols`);
