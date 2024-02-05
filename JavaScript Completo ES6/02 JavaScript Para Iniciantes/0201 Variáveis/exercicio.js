af3e20b24a761ca4a161e5c7d53b49e9b645ad95
Calmon Hebert <102420193+CalmonHebert@users.noreply.github.com>
2024-02-05 20:11:49 -0300

    Create exercicio.js

---
+++ JavaScript Completo ES6/02 JavaScript Para Iniciantes/0201 Variáveis/exercicio.js
@@ -0,0 +1,17 @@
+// Declarar uma variável com o seu nome
+
+let nome = 'Calmon';
+
+// Declarar uma variável com a sua idade
+
+let idade = 34;
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+let comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Pizza'
+
+// Declarar 5 variáveis diferentes sem valores
+let time, banda, sobrenome, musica, bebidaFavorita;
+
