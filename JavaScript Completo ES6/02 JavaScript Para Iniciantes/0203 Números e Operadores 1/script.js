c5e9afc3109f68f9f256a00a7efd1d0eec548278
Calmon Hebert <102420193+CalmonHebert@users.noreply.github.com>
2024-02-05 22:22:04 -0300

    Create script.js

---
+++ JavaScript Completo ES6/02 JavaScript Para Iniciantes/0203 Números e Operadores 1/script.js
@@ -0,0 +1,13 @@
+var numero = 33;
+var exponencial = 2e10
+console.log(numero);
+console.log(exponencial);
+
+var soma = 10 + 5;
+console.log(soma);
+
+var divisão = 10/2;
+console.log(divisão);
+
+var modulo = 14 % 5
+console.log(modulo);
