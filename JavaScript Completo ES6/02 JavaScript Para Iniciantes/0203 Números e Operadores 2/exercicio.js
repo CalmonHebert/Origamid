fd3129f354096d2881d01e4045e993c0fd2bcb01
Calmon Hebert <102420193+CalmonHebert@users.noreply.github.com>
2024-02-06 19:59:55 -0300

    Update exercicio.js

--- JavaScript Completo ES6/02 JavaScript Para Iniciantes/0203 Números e Operadores 2/exercicio.js
+++ JavaScript Completo ES6/02 JavaScript Para Iniciantes/0203 Números e Operadores 2/exercicio.js
@@ -3,13 +3,25 @@ var total = 10 + 5 * 2 / 2 + 20;
 console.log(total);
 
 // Crie duas expressões que retornem NaN
+var n1 = 28
+var n2 = +'2s'
+
+var soma1 = n1 + n2;
+var mult = n1 * n2;
+console.log(soma1);
+console.log(mult);
+
 
 // Somar a string '200' com o número 50 e retornar 250
-
+var nString = '200'
+console.log(+nString + 50);
 // Incremente o número 5 e retorne o seu valor incrementado
+var incremento = 5
+console.log(++incremento);
 
 // Como dividir o peso por 2?
-var numero = '80';
+var numero = '80' / 2;
 var unidade = 'kg';
 var peso = numero + unidade; // '80kg'
-var pesoPorDois = peso / 2; // NaN (Not a Number)
+
+console.log(peso);
