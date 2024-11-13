cf378e697d6b3f000dfaf2b4aca3d8f905e3e502
Calmon Hebert <102420193+CalmonHebert@users.noreply.github.com>
2024-11-13 18:03:58 -0300

    up

---
+++ Html e CSS para Iniciantes/11 JavaScript Básico/1102 Manipular DOM/script.js
@@ -0,0 +1,8 @@
+const nav = document.querySelector('nav')
+const produtos = document.querySelector('.produtos a')
+
+console.log(nav);
+console.log(produtos);
+
+
+
---
+++ Html e CSS para Iniciantes/11 JavaScript Básico/1104 Objetos/script.js
@@ -0,0 +1,21 @@
+const livro = {
+  nome: 'O senhor dos Aneis',
+  ano: 1954,
+  autor: 'J. R. R. Tolkien'
+}
+
+const filme = {
+  nome: 'O senhor dos Aneis',
+  ano: 2001,
+  autor: 'J. R. R. Tolkien',
+  diretor: 'Peter Jackson'
+}
+
+const texto = document.querySelector('p');
+console.log(texto.innerHTML);
+
+console.log(filme.autor.length);
+
+console.log(livro.ano.toFixed(2));
+
+
