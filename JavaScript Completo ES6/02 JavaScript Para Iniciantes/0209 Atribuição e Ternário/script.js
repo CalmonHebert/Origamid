7c35c1a22c21d25cbd10c2aa485acd0cf554d650
Calmon Hebert <102420193+CalmonHebert@users.noreply.github.com>
2024-11-25 17:09:45 -0300

    up

---
+++ JavaScript Completo ES6/02 JavaScript Para Iniciantes/0209 Atribuição e Ternário/script.js
@@ -0,0 +1,25 @@
+var numero = 20;
+var numero2 = 10;
+
+numero += numero2;
+console.log(numero);
+
+// Operador ternário
+var idade = 18;
+var podeBeber = (idade >= 18) ? 'Pode beber' : 'Não pode beber';
+console.log(podeBeber);
+
+// If abreviado
+var possuiFaculdade = true;
+if (possuiFaculdade)
+  console.log('Sim possui');
+else
+  console.log('Não possui');
+
+
+
+
+
+
+
+
---
+++ JavaScript Completo ES6/02 JavaScript Para Iniciantes/0210 Escopo/script.js
@@ -0,0 +1,39 @@
+var carro = 'Fusca';
+
+function mostrarCarro() {
+  console.log(carro);
+}
+mostrarCarro();
+console.log(carro);
+
+function mostrarMoto() {
+  moto = 'titan';
+  console.log(moto);
+}
+mostrarMoto();
+
+if (true) {
+  let mes = 'Dezembro';
+  console.log(mes);
+}
+
+// console.log(mes);
+
+for (var i = 0; i < 10; i++) {
+  console.log(`Numero ${i}`);
+}
+
+console.log(i);
+
+const semana = 'Sexta';
+
+console.log(semana);
+
+const data = {
+  ano: 2018,
+  mes: 'Dezembro',
+}
+
+data.ano = 2019;
+console.log(data.ano);
+
