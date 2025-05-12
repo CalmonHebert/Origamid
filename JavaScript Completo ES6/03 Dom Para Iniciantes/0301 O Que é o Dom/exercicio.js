8c739f081cba66f76e9f77d07d32d4f103bb84e1
Calmon Hebert <102420193+CalmonHebert@users.noreply.github.com>
2025-05-12 13:45:11 -0300

    up

---
+++ JavaScript Completo ES6/03 Dom Para Iniciantes/0301 O Que é o Dom/exercicio.js
@@ -0,0 +1,20 @@
+// Retorne o url da página atual utilizando o objeto window
+const url = window.location.href
+console.log(url);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+const ativo = document.querySelector('.ativo')
+console.log(ativo);
+
+// Retorne a linguagem do navegador
+const linguagem = navigator.language
+console.log(linguagem);
+
+
+// Retorne a largura da janela 
+const largura = innerWidth
+console.log(largura);
+
+
--- JavaScript Completo ES6/03 Dom Para Iniciantes/0301 O Que é o Dom/script.js
+++ JavaScript Completo ES6/03 Dom Para Iniciantes/0301 O Que é o Dom/script.js
@@ -7,8 +7,20 @@ if (href === 'http://127.0.0.1:5500/Origamid/JavaScript%20Completo%20ES6/03%20Do
 }
 
 const h1Selecionado = document.querySelector('h1');
-h1Selecionado = h1Selecionado.classList;
+const h1Classes = h1Selecionado.classList;
 
-h1Selecionado.addEventListener('click', function () {
-  console.log('clicou em ' + h1Selecionado);
-})
+function callbackh1() {
+  console.log('Clicou em ', h1Selecionado.innerText);
+}
+
+h1Selecionado.addEventListener('click', callbackh1)
+console.log('-----Exercício-------');
+
+// Retorne o url da página atual utilizando o objeto window
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+// Retorne a linguagem do navegador
+
+// Retorne a largura da janela 
